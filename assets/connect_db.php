74fc38813b5c2228f5dc47cb4a3ddef9c4294b1f
Evgeny Smirnov <smrnovz@mail.ru>
2021-01-27 21:46:45 +0500

    20.01

--- assets/connect_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db_host = "localhost";
-$db_user = "Evgeny";
-$db_password = "kiloman";
-$db_name = "auth";
-$db_table = "users";
-$connect = new mysqli($db_host,$db_user,$db_password,$db_name);
